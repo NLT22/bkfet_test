cb4395b7389000eb8399eb0c3b4dffdf6451570e
NLT22 <nguyenletrung2002@gmail.com>
2024-04-26 20:10:10 +0700

    Thêm responsive và style cho trang course

--- src/pages/course/course.js
+++ src/pages/course/course.js
@@ -12,77 +12,77 @@ function toggleDropdown(dropdownId) {
 
   dropdownContent.classList.toggle("show");
 }
+
+function openPopup(popupId) {
+  var popup = document.getElementById(popupId);
+  if (popup) {
+    popup.style.display = "flex";
+  }
+}
+
+// Hàm đóng popup
+function closePopup(popupId) {
+  var popup = document.getElementById(popupId);
+  if (popup) {
+    popup.style.display = "none";
+  }
+}
 //
-// function openPopup(popupId) {
-//   var popup = document.getElementById(popupId);
-//   if (popup) {
-//     popup.style.display = "flex";
-//   }
-// }
+$(document).ready(function () {
+  // Duyệt qua mỗi cặp slider và thumbnail
+  $(".content-popup-left").each(function (index) {
+    var sliderId = "carouselExampleDark" + (index + 1); // Tạo ID cho slider
+    var thumbnailId = "carouselThumbnails" + (index + 1); // Tạo ID cho thumbnail
 
-// // Hàm đóng popup
-// function closePopup(popupId) {
-//   var popup = document.getElementById(popupId);
-//   if (popup) {
-//     popup.style.display = "none";
-//   }
-// }
-// //
-// $(document).ready(function () {
-//   // Duyệt qua mỗi cặp slider và thumbnail
-//   $(".content-popup-left").each(function (index) {
-//     var sliderId = "carouselExampleDark" + (index + 1); // Tạo ID cho slider
-//     var thumbnailId = "carouselThumbnails" + (index + 1); // Tạo ID cho thumbnail
+    // Gắn ID cho slider và thumbnail
+    $(this).find(".carousel").attr("id", sliderId);
+    $(this).find(".carousel-thumbnails").attr("id", thumbnailId);
 
-//     // Gắn ID cho slider và thumbnail
-//     $(this).find(".carousel").attr("id", sliderId);
-//     $(this).find(".carousel-thumbnails").attr("id", thumbnailId);
+    // Đặt lớp active cho thumbnail đầu tiên khi trang được load cho slider
+    $("#" + thumbnailId + " .thumbnail")
+      .eq(0)
+      .addClass("active");
 
-//     // Đặt lớp active cho thumbnail đầu tiên khi trang được load cho slider
-//     $("#" + thumbnailId + " .thumbnail")
-//       .eq(0)
-//       .addClass("active");
+    // Đồng bộ hóa khi lướt slide trên cho slider
+    $("#" + sliderId).on("slide.bs.carousel", function (e) {
+      var slideIndex = e.to;
+      $("#" + thumbnailId + " .thumbnail").removeClass("active"); // Xóa lớp active từ tất cả các thumbnail trong slider
+      $("#" + thumbnailId + " .thumbnail")
+        .eq(slideIndex)
+        .addClass("active"); // Thêm lớp active vào thumbnail tương ứng
+    });
 
-//     // Đồng bộ hóa khi lướt slide trên cho slider
-//     $("#" + sliderId).on("slide.bs.carousel", function (e) {
-//       var slideIndex = e.to;
-//       $("#" + thumbnailId + " .thumbnail").removeClass("active"); // Xóa lớp active từ tất cả các thumbnail trong slider
-//       $("#" + thumbnailId + " .thumbnail")
-//         .eq(slideIndex)
-//         .addClass("active"); // Thêm lớp active vào thumbnail tương ứng
-//     });
+    // Đồng bộ hóa khi click vào thumbnail dưới cho slider
+    $("#" + thumbnailId + " .thumbnail").click(function () {
+      var slideIndex = $(this).data("bs-slide-to");
+      $("#" + sliderId).carousel(slideIndex);
+      $("#" + thumbnailId + " .thumbnail").removeClass("active"); // Xóa lớp active từ tất cả các thumbnail trong slider
+      $(this).addClass("active"); // Thêm lớp active vào thumbnail được chọn
+    });
+  });
 
-//     // Đồng bộ hóa khi click vào thumbnail dưới cho slider
-//     $("#" + thumbnailId + " .thumbnail").click(function () {
-//       var slideIndex = $(this).data("bs-slide-to");
-//       $("#" + sliderId).carousel(slideIndex);
-//       $("#" + thumbnailId + " .thumbnail").removeClass("active"); // Xóa lớp active từ tất cả các thumbnail trong slider
-//       $(this).addClass("active"); // Thêm lớp active vào thumbnail được chọn
-//     });
-//   });
+  // Đồng bộ hóa khi ảnh lớn thay đổi thì ảnh nhỏ dưới cũng thay đổi theo
+  $(".carousel").on("slid.bs.carousel", function (e) {
+    var activeIndex = $(this).find(".carousel-item.active").index();
+    var thumbnails = $(this).next(".carousel-thumbnails").find(".thumbnail");
+    thumbnails.removeClass("active");
+    thumbnails.eq(activeIndex).addClass("active");
+  });
+});
+//
+for (let i = 1; i <= 3; i++) {
+  document
+    .getElementById(`showPopup${i}`)
+    .addEventListener("click", function () {
+      document.getElementById(`popuppopup${i}`).style.display = "block";
+    });
 
-//   // Đồng bộ hóa khi ảnh lớn thay đổi thì ảnh nhỏ dưới cũng thay đổi theo
-//   $(".carousel").on("slid.bs.carousel", function (e) {
-//     var activeIndex = $(this).find(".carousel-item.active").index();
-//     var thumbnails = $(this).next(".carousel-thumbnails").find(".thumbnail");
-//     thumbnails.removeClass("active");
-//     thumbnails.eq(activeIndex).addClass("active");
-//   });
-// });
-// //
-// for (let i = 1; i <= 3; i++) {
-//   document
-//     .getElementById(`showPopup${i}`)
-//     .addEventListener("click", function () {
-//       document.getElementById(`popuppopup${i}`).style.display = "block";
-//     });
-
-//   document
-//     .getElementById(`closePopupPopup${i}`)
-//     .addEventListener("click", function () {
-//       document.getElementById(`popuppopup${i}`).style.display = "none";
-//     });
-// }
+  document
+    .getElementById(`closePopupPopup${i}`)
+    .addEventListener("click", function () {
+      document.getElementById(`popuppopup${i}`).style.display = "none";
+    });
+}
 
 
 
